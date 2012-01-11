a709dd0d7cf2a007d3b11cef065ec2b822d0c039
Mattias Lyckne <olyckne@gmail.com>
2012-01-11 21:42:53 +0100

    Bugfixes

--- system/core/interfaces/Active.php
+++ system/core/interfaces/Active.php
@@ -1,12 +1,12 @@
 <?php
 
+
 /**
  * Just a interface used to tell if a controller is enabled or not
- *
  *
  * @package default
  * @author 
  **/
 interface Active {
-
-} // END interface active
+	
+} // END interface Active
